8af4695ff86620f8dab3097a088c49ab7aad2d9b
vagrant <vagrant@xeno.dev>
2017-08-09 18:27:46 +0000

    Adds theme tests

---
+++ wp-content/themes/wordpress-bootstrap-child/tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Wordpress_Bootstrap_Child
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}

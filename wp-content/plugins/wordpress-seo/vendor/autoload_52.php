178042b9a2c4aaaa91324bb6f31c29272076fb91
Jim Birch <hello@jimbir.ch>
2017-09-20 14:19:57 -0400

    Updates Plugins

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit072f4fc627df93445aa7d4702d387818::getLoader();
+return ComposerAutoloaderInit8f92b04ef3329b35bd540380b13d64ae::getLoader();
